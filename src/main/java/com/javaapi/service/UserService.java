c6a8e6f35ac58ceb24a57059b1230bcb343de92e
Adrian <adrian.talonia11@gmail.com>
2022-12-19 22:32:20 -0600

    add some changes

--- src/main/java/com/javaapi/service/UserService.java
+++ src/main/java/com/javaapi/service/UserService.java
@@ -1,4 +1,12 @@
 package com.javaapi.service;
 
+import com.javaapi.model.dto.UserRequest;
+import com.javaapi.model.dto.UserResponse;
+
 public interface UserService {
+
+    UserResponse create(UserRequest user);
+    UserResponse update(UserRequest user);
+
+
 }
--- src/main/java/com/javaapi/service/UserServiceImpl.java
+++ src/main/java/com/javaapi/service/UserServiceImpl.java
@@ -1,4 +1,26 @@
 package com.javaapi.service;
 
-public class UserServiceImpl {
+import com.javaapi.model.dto.UserRequest;
+import com.javaapi.model.dto.UserResponse;
+import com.javaapi.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserResponse create(UserRequest user) {
+        return null;
+    }
+
+    @Override
+    public UserResponse update(UserRequest user) {
+        return null;
+    }
 }

104b09c284c6ab18c3f296d925e7cb75c1657b62
Adrian <adrian.talonia11@gmail.com>
2022-12-22 22:39:43 -0600

    add some changes

--- src/main/java/com/javaapi/model/dto/UserRequest.java
+++ src/main/java/com/javaapi/model/dto/UserRequest.java
@@ -2,13 +2,14 @@ package com.javaapi.model.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Data
 public class UserRequest {
 
-    @NotNull(message = "First name cannot be null")
+    @NotBlank(message = "First name cannot be blank")
     @Size(min = 4, message = "Firstname must not be less than two characters")
     private String firstName;
     @NotNull(message = "Last name cannot be null")
--- src/main/java/com/javaapi/model/dto/UserResponse.java
+++ src/main/java/com/javaapi/model/dto/UserResponse.java
@@ -4,4 +4,6 @@ import lombok.Data;
 
 @Data
 public class UserResponse {
+    private String firstName;
+    private String lastName;
 }

0ab539a87776f64505b7e5cf1402689dfbf33a18
Adrian <adrian.talonia11@gmail.com>
2023-01-03 22:30:32 -0600

    add role class

--- src/main/java/com/javaapi/model/entity/Role.java
+++ src/main/java/com/javaapi/model/entity/Role.java
@@ -1,4 +1,21 @@
 package com.javaapi.model.entity;
 
-public class Role {
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
+@Table(name = "roles", schema = "public")
+public class Role extends BaseEntity {
+
+    private String name;
+    private String description;
 }
